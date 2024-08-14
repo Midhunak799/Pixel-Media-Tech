4027b33c260dbb66da514488a675dc836fb2ffdb
Midhun <midhunak799@gmail.com>
2024-08-14 11:22:20 +0530

    first commit

---
+++ login.js
@@ -0,0 +1,16 @@
+function showLogin() {
+    document.getElementById('loginForm').classList.add('active');
+    document.getElementById('signupForm').classList.remove('active');
+    document.getElementById('loginBtn').classList.add('active');
+    document.getElementById('signupBtn').classList.remove('active');
+}
+
+function showSignup() {
+    document.getElementById('signupForm').classList.add('active');
+    document.getElementById('loginForm').classList.remove('active');
+    document.getElementById('signupBtn').classList.add('active');
+    document.getElementById('loginBtn').classList.remove('active');
+}
+
+
+
